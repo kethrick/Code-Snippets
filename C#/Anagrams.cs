018715a42f6aa0f6333cfae69a05e7d2d40121c4
kethrick <kethrick@gmail.com>
2017-07-24 20:16:57 -0700

    More snippets
    Added palindrome, anagram in c#

---
+++ C#/Anagrams.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Anagrams
+{
+    class Anagrams
+    {
+        static void Main(string[] args)
+        {
+            // Read and sort dictionary
+            var d = Read();
+
+            // Read user input and show anagrams
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                Show(d, line);
+            }
+        }
+
+        static Dictionary<string, string> Read()
+        {
+            var d = new Dictionary<string, string>();
+
+            // Read each line.
+            FileStream fs = new FileStream("anagram.txt", FileMode.Open, FileAccess.Read);
+            using (StreamReader r = new StreamReader(fs))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null)
+                {
+                    // Alphabetize the line for the key.
+                    // Then add to the value string.
+                    string a = Alphabetize(line);
+                    string v;
+                    if(d.TryGetValue(a, out v))
+                    {
+                        d[a] = v + ", " + line;
+                    }
+                    else
+                    {
+                        d.Add(a, line);
+                    }
+                }
+            }
+            return d;
+        }
+
+        static string Alphabetize(string s)
+        {
+            // convert to a char array, then sort and return
+            char[] a = s.ToCharArray();
+            Array.Sort(a);
+            return new string(a);
+        }
+
+        static void Show(Dictionary<string, string> d, string w)
+        {
+            // Write value for alphabetized word
+            string v;
+            if(d.TryGetValue(Alphabetize(w), out v))
+            {
+                Console.WriteLine(v);
+            }
+            else
+            {
+                Console.WriteLine("-");
+            }
+        }
+    }
+}
---
+++ C#/Palindrome.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Palindrome
+{
+    public class Palindrome
+    {
+        public static bool IsPalindrome(string value)
+        {
+            int min = 0;
+            int max = value.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+
+                char a = value[min];
+                char b = value[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] array =
+            {
+                "civic",
+                "deified",
+                "deleveled",
+                "devoved",
+                "dewed",
+                "Hannah",
+                "kayak",
+                "level",
+                "madam",
+                "racecar",
+                "radar",
+                "redder",
+                "refer",
+                "repaper",
+                "reviver",
+                "rotator",
+                "rotor",
+                "sagas",
+                "solos",
+                "sexes",
+                "stats",
+                "tenet",
+
+                "Dot",
+                "Net",
+                "Perls",
+                "Is",
+                "Not",
+                "A",
+                "Palindrome",
+                ""
+            };
+            foreach (string value in array)
+            {
+                Console.WriteLine("{0} = {1}", value, IsPalindrome(value));
+            }
+        }
+    }
+
+}
